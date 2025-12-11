3ab57f247caa25d098845c67eaf81047496657ec
Jon Baldwin Almarez <88573999+JonBaldwinAlmarez@users.noreply.github.com>
2025-12-11 14:54:00 +0800

    Import pandas and load penguins dataset
    Added pandas to read a CSV file of penguins data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Penguin Identifier')
 
 st.info('This is a machine learning app')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+df
