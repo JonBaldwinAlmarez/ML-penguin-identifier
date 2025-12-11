e9f49d8f25b21e954757c173d5238896ec4596d3
Jon Baldwin Almarez <88573999+JonBaldwinAlmarez@users.noreply.github.com>
2025-12-11 14:58:47 +0800

    Implement data expander for raw data display
    Added an expander to display raw data in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ st.title('Penguin Identifier')
 
 st.info('This is a machine learning app')
 
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-
-df
+with st.expander("Data"):
+  st.write("**Raw Data**")
+  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+  
+  df
