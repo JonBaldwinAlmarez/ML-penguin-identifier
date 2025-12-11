9aa3964c59251ba0144429ab06599a54e2e3bb73
Jon Baldwin Almarez <88573999+JonBaldwinAlmarez@users.noreply.github.com>
2025-12-11 15:04:33 +0800

    Separate features X and target y from DataFrame
    Added code to separate features and target variable from the dataset.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,12 @@ st.info('This is a machine learning app')
 with st.expander("Data"):
   st.write("**Raw Data**")
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-  
   df
+  
+  st.write("**X**")
+  X = df.drop("species", axis=1)
+  X
+
+  st.write("**y**")
+  y= df.species
+  y
