8fe4a4fec46b0ffaf23ff1df40c44cb8da98af78
Jon Baldwin Almarez <88573999+JonBaldwinAlmarez@users.noreply.github.com>
2025-12-12 20:28:47 +0800

    Add DataFrame for prediction probabilities
    Create a DataFrame from prediction probabilities and rename columns for clarity.

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,6 +93,10 @@ clf.fit(X, y)    #  Train the ML model
 
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
-prediction_proba
+
+df_prediction_proba = pd.Dataframe(prediction_proba)
+df_prediction_proba.columns = ["Adelie","Chinstrip","Gentoo"]
+df_prediction_proba.rename(columns = {0: "Adelie", 1:"Chinstrip", 2:"Gentoo"})
+df_prediction_proba
 
 
